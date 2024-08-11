3234c83f04ffa2fe403bc467161a962e26ec48f9
carlos99at <carlos.abia99@gmail.com>
2024-08-11 20:51:24 +0200

    feat: onclose added

--- src/CardsApp.tsx
+++ src/CardsApp.tsx
@@ -1,28 +1,41 @@
-import { Button } from "@nextui-org/react"
-import { GameModal } from "./components/GameModal"
-import { useState } from "react"
+import { Button } from '@nextui-org/react'
+import { GameModal } from './components/GameModal'
+import { useState } from 'react'
 
 export const CardsApp = () => {
   const [openModal, setOpenModal] = useState<'ENTER' | 'CREATE'>()
   const modals = {
     ENTER: {
       message: 'Introduce el código de tu mesa',
-      acceptAction: () => { },
+      acceptAction: () => {}
     },
     CREATE: {
       message: 'Introduce un nombre para crear una mesa',
-      acceptAction: () => { },
+      acceptAction: () => {}
     }
   }
 
   const buttonClick = (modal: 'CREATE' | 'ENTER') => {
     setOpenModal(modal)
   }
-  return <>
-    <Button size="md" variant="solid" onClick={() => buttonClick('ENTER')}>Entra en la partida</Button>
-    <Button size="md" variant="solid" onClick={() => buttonClick('CREATE')}>Crea una partida nueva</Button>
-    {openModal && Object.keys(modals).map(modal =>
-      <GameModal key={modal} isOpen={openModal === modal} title={modals[openModal].message} acceptAction={modals[openModal].acceptAction}></GameModal>
-    )}
-  </>
+  return (
+    <>
+      <Button size='md' variant='solid' onClick={() => buttonClick('ENTER')}>
+        Entra en la partida
+      </Button>
+      <Button size='md' variant='solid' onClick={() => buttonClick('CREATE')}>
+        Crea una partida nueva
+      </Button>
+      {openModal &&
+        Object.keys(modals).map(modal => (
+          <GameModal
+            key={modal}
+            isOpen={openModal === modal}
+            title={modals[openModal].message}
+            acceptAction={modals[openModal].acceptAction}
+            onClose={() => setOpenModal(undefined)}
+          ></GameModal>
+        ))}
+    </>
+  )
 }
--- src/components/GameModal.tsx
+++ src/components/GameModal.tsx
@@ -1,33 +1,41 @@
-import { ModalContent, ModalHeader, ModalBody, ModalFooter, Button, Modal, Input } from "@nextui-org/react"
+import {
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  Modal,
+  Input
+} from '@nextui-org/react'
 
 type GameModalProps = {
-  isOpen: boolean;
-  title: string;
-  acceptAction: () => void;
+  isOpen: boolean
+  title: string
+  acceptAction: () => void
+  onClose: () => void
 }
-export const GameModal = ({isOpen, title, acceptAction }: GameModalProps) => {
-    return <Modal isOpen={isOpen} >
-    <ModalContent>
-      {(onClose) => (
-        <>
-          <ModalHeader className="flex flex-col gap-1">{title}</ModalHeader>
-          <ModalBody>
-          <Input
-      label='Nombre de la mesa'
-      type='text'
-      onInput={() => {}}
-    />
-          </ModalBody>
-          <ModalFooter>
-            <Button color="danger" variant="light" onPress={onClose}>
-              Volver
-            </Button>
-            <Button color="primary" onPress={acceptAction}>
-              Aceptar
-            </Button>
-          </ModalFooter>
-        </>
-      )}
-    </ModalContent>
-  </Modal>
+export const GameModal = ({
+  isOpen,
+  title,
+  acceptAction,
+  onClose
+}: GameModalProps) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalContent>
+        <ModalHeader className='flex flex-col gap-1'>{title}</ModalHeader>
+        <ModalBody>
+          <Input label='Nombre de la mesa' type='text' onInput={() => {}} />
+        </ModalBody>
+        <ModalFooter>
+          <Button color='danger' variant='light' onPress={onClose}>
+            Volver
+          </Button>
+          <Button color='primary' onPress={acceptAction}>
+            Aceptar
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  )
 }
